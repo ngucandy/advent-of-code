23cadfdd484ebdc56c7a7317d1334c9975e4e188
Andy Nguyen <anguyen@computer.org>
2024-12-01 18:07:27 -0500

    aoc2023 day 1 part 2

--- aoc2023/day1.go
+++ aoc2023/day1.go
@@ -5,8 +5,28 @@ import (
 	"log/slog"
 	"os"
 	"regexp"
-	"strconv"
 )
+
+var table = map[string]int{
+	"1":     1,
+	"2":     2,
+	"3":     3,
+	"4":     4,
+	"5":     5,
+	"6":     6,
+	"7":     7,
+	"8":     8,
+	"9":     9,
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
 
 func main() {
 	infile := os.Args[1]
@@ -22,22 +42,32 @@ func main() {
 		}
 	}(file)
 
+	// watch out for these: `oneight`, `eightwo`, `twone`
+	releft := regexp.MustCompile("([\\d]|one|two|three|four|five|six|seven|eight|nine).*")
+	reright := regexp.MustCompile(".*([\\d]|one|two|three|four|five|six|seven|eight|nine)")
+
 	sum := 0
-	re := regexp.MustCompile("[0-9]")
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		digits := re.FindAllString(line, -1)
-		digit, err := strconv.ParseInt(digits[0], 10, 32)
-		if err != nil {
-			panic(err)
+
+		match := releft.FindStringSubmatch(line)
+		first := match[1]
+		digit, ok := table[first]
+		if !ok {
+			panic(first)
 		}
-		val := 10 * int(digit)
-		digit, err = strconv.ParseInt(digits[len(digits)-1], 10, 32)
-		if err != nil {
-			panic(err)
+		val := 10 * digit
+
+		match = reright.FindStringSubmatch(line)
+		last := match[1]
+		digit, ok = table[last]
+		if !ok {
+			panic(last)
 		}
-		val += int(digit)
+		val += digit
+
+		//slog.Info("Digits:", "line", line, "first", first, "last", last, "val", val)
 		sum += val
 	}
 	slog.Info("Sum of calibration values:", "sum", sum)
