ea3421ecdd7b7c2287c4906277a944c51bc152f9
Andy Nguyen <anguyen@computer.org>
2024-12-01 19:31:08 -0500

    refactor part 1

--- aoc2023/day2.go
+++ aoc2023/day2.go
@@ -9,11 +9,17 @@ import (
 	"strings"
 )
 
-var allCubes = map[string]int{
-	"red":   12,
-	"green": 13,
-	"blue":  14,
-}
+var (
+	reDelimReveals = regexp.MustCompile("[:;]")
+	reDelimCubes   = regexp.MustCompile("[,]")
+	reGame         = regexp.MustCompile("Game ([\\d]+)")
+
+	allCubes = map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+)
 
 func main() {
 	infile := os.Args[1]
@@ -29,13 +35,9 @@ func main() {
 		}
 	}(file)
 
-	reDelimReveals := regexp.MustCompile("[:;]")
-	reDelimCubes := regexp.MustCompile("[,]")
-	reGame := regexp.MustCompile("Game ([\\d]+)")
 	sum := 0
 	scanner := bufio.NewScanner(file)
 
-GameLoop:
 	for scanner.Scan() {
 		line := scanner.Text()
 		reveals := reDelimReveals.Split(line, -1)
@@ -47,29 +49,37 @@ GameLoop:
 		}
 		slog.Info("Game:", "ID", id)
 
-		for _, reveal := range reveals[1:] {
-			reveal = strings.TrimSpace(reveal)
-			cubes := reDelimCubes.Split(reveal, -1)
-			for _, cube := range cubes {
-				cube = strings.TrimSpace(cube)
-				split := strings.Split(cube, " ")
-				n, err := strconv.ParseInt(split[0], 10, 32)
-				if err != nil {
-					panic(err)
-				}
-				max, ok := allCubes[split[1]]
-				if !ok {
-					panic(line)
-				}
-				if max-int(n) < 0 {
-					slog.Info("Impossible game:", "ID", id, "cube", cube)
-					continue GameLoop
-				}
-			}
+		if isGamePossible(reveals) {
+			slog.Info("Possible game")
+			sum += int(id)
+		} else {
+			slog.Info("Impossible game")
 		}
-		slog.Info("Possible game:", "ID", id)
-		sum += int(id)
 	}
 
 	slog.Info("Sum of possible game IDs:", "sum", sum)
+}
+
+func isGamePossible(reveals []string) bool {
+	for _, reveal := range reveals[1:] {
+		reveal = strings.TrimSpace(reveal)
+		cubes := reDelimCubes.Split(reveal, -1)
+		for _, cube := range cubes {
+			cube = strings.TrimSpace(cube)
+			split := strings.Split(cube, " ")
+			n, err := strconv.ParseInt(split[0], 10, 32)
+			if err != nil {
+				panic(err)
+			}
+			maxCubes, ok := allCubes[split[1]]
+			if !ok {
+				panic(reveals)
+			}
+			if maxCubes-int(n) < 0 {
+				slog.Info("Impossible game:", "cube", cube)
+				return false
+			}
+		}
+	}
+	return true
 }
