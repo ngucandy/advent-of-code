33c2307b2e1358102242d055662ef91c9c222993
Andy Nguyen <anguyen@computer.org>
2024-12-02 20:22:32 -0500

    day 3 part 2

--- aoc2023/day3.go
+++ aoc2023/day3.go
@@ -11,6 +11,7 @@ import (
 var (
 	rexpPart   = regexp.MustCompile(`\d+`)
 	rexpSymbol = regexp.MustCompile(`[^.\d]`)
+	rexpGear   = regexp.MustCompile(`\*`)
 )
 
 func main() {
@@ -27,7 +28,7 @@ func main() {
 		}
 	}(file)
 
-	schematic, parts := buildSchematic(file)
+	schematic, parts, gears := buildSchematic(file)
 
 	sum := 0
 	for _, part := range parts {
@@ -36,11 +37,18 @@ func main() {
 		}
 	}
 	slog.Info("Sum of all part numbers:", "sum", sum)
+
+	sum = 0
+	for _, gear := range gears {
+		sum += computeRatio(gear, schematic)
+	}
+	slog.Info("Sum of all gear ratios:", "sum", sum)
 }
 
-func buildSchematic(file *os.File) ([][]int, []Part) {
+func buildSchematic(file *os.File) ([][]int, []Part, []Gear) {
 	var schematic [][]int
 	var parts []Part
+	var gears []Gear
 	y := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -49,23 +57,32 @@ func buildSchematic(file *os.File) ([][]int, []Part) {
 
 		symbolMatches := rexpSymbol.FindAllStringSubmatchIndex(line, -1)
 		for _, match := range symbolMatches {
-			row[match[0]] = 1
+			row[match[0]] = -1
+		}
+
+		gearMatches := rexpGear.FindAllStringSubmatchIndex(line, -1)
+		for _, match := range gearMatches {
+			g := Gear{
+				x: match[0],
+				y: y,
+			}
+			gears = append(gears, g)
 		}
 
 		partMatches := rexpPart.FindAllStringSubmatchIndex(line, -1)
 		for _, match := range partMatches {
-			for i := range match[1] - match[0] {
-				row[match[0]+i] = 2
-			}
-			n, err := strconv.ParseInt(line[match[0]:match[1]], 10, 32)
+			n, err := strconv.Atoi(line[match[0]:match[1]])
 			if err != nil {
 				panic(err)
+			}
+			for i := range match[1] - match[0] {
+				row[match[0]+i] = n
 			}
 			p := Part{
 				x:   match[0],
 				xx:  match[1],
 				y:   y,
-				val: int(n),
+				val: n,
 			}
 			parts = append(parts, p)
 		}
@@ -73,13 +90,13 @@ func buildSchematic(file *os.File) ([][]int, []Part) {
 		schematic = append(schematic, row)
 		y++
 	}
-	return schematic, parts
+	return schematic, parts, gears
 }
 
 func isSymbolAdjacent(part Part, schematic [][]int) bool {
 	for j := max(0, part.y-1); j <= min(len(schematic)-1, part.y+1); j++ {
 		for i := max(0, part.x-1); i <= min(len(schematic[j])-1, part.xx); i++ {
-			if schematic[j][i] == 1 {
+			if schematic[j][i] == -1 {
 				return true
 			}
 		}
@@ -87,9 +104,40 @@ func isSymbolAdjacent(part Part, schematic [][]int) bool {
 	return false
 }
 
+func computeRatio(gear Gear, schematic [][]int) int {
+	ratio := 0
+	adjParts := getAdjacentParts(gear, schematic)
+	if len(adjParts) == 2 {
+		ratio = adjParts[0] * adjParts[1]
+	}
+	return ratio
+}
+
+func getAdjacentParts(gear Gear, schematic [][]int) []int {
+	m := make(map[int]bool)
+	for j := max(0, gear.y-1); j <= min(len(schematic)-1, gear.y+1); j++ {
+		for i := max(0, gear.x-1); i <= min(len(schematic[j])-1, gear.x+1); i++ {
+			if schematic[j][i] > 0 {
+				m[schematic[j][i]] = true
+			}
+		}
+	}
+
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
 type Part struct {
 	x   int
 	xx  int
 	y   int
 	val int
+}
+
+type Gear struct {
+	x int
+	y int
 }
