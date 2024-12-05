35affda687ce0eacdcbc7444efebda882e3c62a5
Andy Nguyen <anguyen@computer.org>
2024-12-05 09:56:35 -0500

    day 5 part 2

--- aoc2024/cmd/day5/main.go
+++ aoc2024/cmd/day5/main.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"os"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -41,6 +42,7 @@ func main() {
 	}
 
 	part1(mapRules, pageUpdates)
+	part2(mapRules, pageUpdates)
 }
 
 func part1(rules map[string]bool, updates [][]string) {
@@ -65,4 +67,45 @@ UpdatesLoop:
 		sum += n
 	}
 	slog.Info("Part 1:", "sum", sum)
+}
+
+func part2(rules map[string]bool, updates [][]string) {
+	sum := 0
+	for _, update := range updates {
+		slog.Info("Processing update:", "update", update)
+		violation := false
+	UpdateLoop:
+		for i := range update {
+			for a := range i {
+				r := update[i] + "|" + update[a]
+				if _, ok := rules[r]; ok {
+					slog.Info("Violation:", "rule", r)
+					violation = true
+					slices.SortStableFunc(update, func(a, b string) int {
+						if _, ok := rules[a+"|"+b]; ok {
+							return -1
+						}
+						if _, ok := rules[b+"|"+a]; ok {
+							return 1
+						}
+						return 0
+					})
+					slog.Info("Sorted:", "update", update)
+					break UpdateLoop
+				}
+			}
+		}
+		if !violation {
+			slog.Info("No violation:")
+			continue
+		}
+
+		n, err := strconv.Atoi(update[len(update)/2])
+		if err != nil {
+			panic(err)
+		}
+		slog.Info("Corrected:", "middle", n)
+		sum += n
+	}
+	slog.Info("Part 2:", "sum", sum)
 }
