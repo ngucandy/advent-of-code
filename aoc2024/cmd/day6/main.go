5c503c98aa7f7f7a826f6a07c7d74f12d3a43488
Andy Nguyen <anguyen@computer.org>
2024-12-06 16:03:16 -0500

    day 6 part 2

--- aoc2024/cmd/day6/main.go
+++ aoc2024/cmd/day6/main.go
@@ -33,13 +33,17 @@ func main() {
 	rexpObstruction := regexp.MustCompile(`#`)
 	rexpGuard := regexp.MustCompile(`\^`)
 	obstructions := [][]int{}
-	guardLoc := [2]int{-1, -1}
+	guardLoc := []int{-1, -1}
 	direction := 0
 	visited := [][]int{}
 	y := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
+		if len(line) == 0 {
+			continue
+		}
+
 		visited = append(visited, make([]int, len(line)))
 
 		// locate obstructions
@@ -54,17 +58,19 @@ func main() {
 		if loc != nil {
 			guardLoc[0] = loc[0]
 			guardLoc[1] = y
-			visited[y][loc[0]] = 1
 		}
 
 		y++
 	}
+
 	part1(obstructions, guardLoc, direction, visited)
+	part2(obstructions, guardLoc, direction, visited)
 }
 
-func part1(obstructions [][]int, guardLoc [2]int, d int, visited [][]int) {
+func part1(obstructions [][]int, guardLoc []int, d int, visited [][]int) {
+	visited[guardLoc[1]][guardLoc[0]] = 1
 	for {
-		newLoc := [2]int{guardLoc[0] + directions[d][0], guardLoc[1] + directions[d][1]}
+		newLoc := []int{guardLoc[0] + directions[d][0], guardLoc[1] + directions[d][1]}
 
 		if newLoc[0] < 0 || newLoc[0] == len(visited[0]) || newLoc[1] < 0 || newLoc[1] == len(visited) {
 			break
@@ -72,7 +78,6 @@ func part1(obstructions [][]int, guardLoc [2]int, d int, visited [][]int) {
 
 		if obstructions[newLoc[1]][newLoc[0]] == 1 {
 			d = (d + 1) % len(directions)
-			slog.Info("Changed direction:", "new direction", d)
 			continue
 		}
 
@@ -87,5 +92,50 @@ func part1(obstructions [][]int, guardLoc [2]int, d int, visited [][]int) {
 		}
 	}
 
-	slog.Info("Total visited:", "total", total)
+	slog.Info("Part 1:", "total", total)
+}
+
+func part2(obstructions [][]int, guardLocStart []int, d int, visitedBase [][]int) {
+	total := 0
+	for y := range len(visitedBase) {
+		for x := range len(visitedBase[y]) {
+			if obstructions[y][x] == 1 {
+				continue
+			}
+			obstructions[y][x] = 1
+			guardLoc := []int{guardLocStart[0], guardLocStart[1]}
+			visited := make([][]int, len(visitedBase))
+			for i := range visited {
+				visited[i] = make([]int, len(visitedBase[i]))
+			}
+			visited[guardLocStart[1]][guardLocStart[0]] = 1 + d
+			if isStuck(obstructions, guardLoc, d, visited) {
+				total++
+			}
+			obstructions[y][x] = 0
+		}
+	}
+	slog.Info("Part 2:", "total", total)
+}
+
+func isStuck(obstructions [][]int, guardLoc []int, d int, visited [][]int) bool {
+	for {
+		newLoc := []int{guardLoc[0] + directions[d][0], guardLoc[1] + directions[d][1]}
+
+		if newLoc[0] < 0 || newLoc[0] == len(visited[0]) || newLoc[1] < 0 || newLoc[1] == len(visited) {
+			return false
+		}
+
+		if obstructions[newLoc[1]][newLoc[0]] == 1 {
+			d = (d + 1) % len(directions)
+			continue
+		}
+
+		if d == visited[newLoc[1]][newLoc[0]]-1 {
+			return true
+		}
+
+		guardLoc = newLoc
+		visited[guardLoc[1]][guardLoc[0]] = 1 + d
+	}
 }
