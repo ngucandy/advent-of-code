ba0943731e948621daee7cb50386daebb40f4f74
Andy Nguyen <anguyen@computer.org>
2024-12-10 17:33:04 -0500

    day 5 part 2 partial

--- aoc2023/day5.go
+++ aoc2023/day5.go
@@ -1,5 +1,9 @@
 package aoc2023
 
+import (
+	"slices"
+)
+
 type Mapper struct {
 	Src      string
 	Dst      string
@@ -12,11 +16,68 @@ func NewMapper(src string, dst string) *Mapper {
 
 func (m *Mapper) Map(src int) (int, string) {
 	for _, r := range m.mappings {
-		if src >= r.srcStart && src < r.srcStart+r.length {
+		if src >= r.srcStart && src < r.srcEnd {
 			return r.dstStart + (src - r.srcStart), m.Dst
 		}
 	}
 	return src, m.Dst
+}
+
+func (m *Mapper) MapRange(srcStart int, length int) ([][]int, string) {
+	var dstRanges [][]int
+
+	// chop up src range into multiple ranges
+	srcEnd := srcStart + length
+	currentStart := srcStart
+	var srcMappings []Mapping
+	slices.SortFunc(m.mappings, func(a, b Mapping) int {
+		if a.srcEnd < b.srcEnd {
+			return -1
+		}
+		if a.srcEnd > b.srcEnd {
+			return 1
+		}
+		return 0
+	})
+	for _, r := range m.mappings {
+		if r.srcEnd <= srcStart || r.srcStart >= srcEnd {
+			continue
+		}
+		if r.srcEnd <= srcEnd {
+			if currentStart < r.srcStart {
+				srcMapping := NewMapping(currentStart, currentStart, r.srcStart-currentStart)
+				srcMappings = append(srcMappings, srcMapping)
+				currentStart = r.srcStart
+			}
+			srcMapping := NewMapping(currentStart, r.dstStart+(currentStart-r.srcStart), r.srcEnd-currentStart)
+			srcMappings = append(srcMappings, srcMapping)
+			currentStart = r.srcEnd
+			continue
+		}
+		if r.srcStart < srcEnd {
+			if currentStart < r.srcStart {
+				srcMapping := NewMapping(currentStart, currentStart, r.srcStart-currentStart)
+				srcMappings = append(srcMappings, srcMapping)
+				currentStart = r.srcStart
+			}
+			srcMapping := NewMapping(currentStart, r.dstStart, srcEnd-currentStart)
+			srcMappings = append(srcMappings, srcMapping)
+			currentStart = srcEnd
+			continue
+		}
+	}
+	// deal with remainder of original source range
+	if currentStart < srcEnd {
+		srcMapping := NewMapping(currentStart, currentStart, srcEnd-currentStart)
+		srcMappings = append(srcMappings, srcMapping)
+	}
+
+	for _, srcMapping := range srcMappings {
+		dstRange := []int{srcMapping.dstStart, srcMapping.dstEnd - srcMapping.dstStart}
+		dstRanges = append(dstRanges, dstRange)
+	}
+
+	return dstRanges, m.Dst
 }
 
 func (m *Mapper) AddMapping(mapping Mapping) {
@@ -25,10 +86,16 @@ func (m *Mapper) AddMapping(mapping Mapping) {
 
 type Mapping struct {
 	srcStart int
+	srcEnd   int
 	dstStart int
-	length   int
+	dstEnd   int
 }
 
 func NewMapping(srcStart int, dstStart int, length int) Mapping {
-	return Mapping{srcStart: srcStart, dstStart: dstStart, length: length}
+	return Mapping{
+		srcStart: srcStart,
+		srcEnd:   srcStart + length,
+		dstStart: dstStart,
+		dstEnd:   dstStart + length,
+	}
 }
--- aoc2023/day5_test.go
+++ aoc2023/day5_test.go
@@ -1,6 +1,9 @@
 package aoc2023
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestMapping_Map(t *testing.T) {
 	type args struct {
@@ -39,6 +42,144 @@ func TestMapping_Map(t *testing.T) {
 			m.AddMapping(NewMapping(20, 10, 10))
 			if got, _ := m.Map(tt.args.src); got != tt.want {
 				t.Errorf("Map() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMapper_MapRange(t *testing.T) {
+	type fields struct {
+		Src      string
+		Dst      string
+		mappings []Mapping
+	}
+	type args struct {
+		srcStart int
+		length   int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   [][]int
+		want1  string
+	}{
+		{
+			name: "MapRange no overlap",
+			fields: fields{
+				Src: "src",
+				Dst: "dst",
+				mappings: []Mapping{
+					NewMapping(10, 20, 10),
+					NewMapping(30, 40, 10),
+				},
+			},
+			args: args{
+				srcStart: 20,
+				length:   10,
+			},
+			want: [][]int{
+				{20, 10},
+			},
+			want1: "dst",
+		},
+		{
+			name: "MapRange full overlap",
+			fields: fields{
+				Src: "src",
+				Dst: "dst",
+				mappings: []Mapping{
+					NewMapping(10, 20, 10),
+				},
+			},
+			args: args{
+				srcStart: 10,
+				length:   10,
+			},
+			want: [][]int{
+				{20, 10},
+			},
+			want1: "dst",
+		},
+		{
+			name: "MapRange full overlap with multiple mappings",
+			fields: fields{
+				Src: "src",
+				Dst: "dst",
+				mappings: []Mapping{
+					NewMapping(10, 20, 5),
+					NewMapping(15, 30, 5),
+				},
+			},
+			args: args{
+				srcStart: 10,
+				length:   10,
+			},
+			want: [][]int{
+				{20, 5},
+				{30, 5},
+			},
+			want1: "dst",
+		},
+		{
+			name: "MapRange overlap with disjoint mappings",
+			fields: fields{
+				Src: "src",
+				Dst: "dst",
+				mappings: []Mapping{
+					NewMapping(10, 110, 10),
+					NewMapping(30, 130, 10),
+				},
+			},
+			args: args{
+				srcStart: 15,
+				length:   20,
+			},
+			want: [][]int{
+				{115, 5},
+				{20, 10},
+				{130, 5},
+			},
+			want1: "dst",
+		},
+		{
+			name: "MapRange overlap with embedded disjoint mappings",
+			fields: fields{
+				Src: "src",
+				Dst: "dst",
+				mappings: []Mapping{
+					NewMapping(10, 110, 10),
+					NewMapping(23, 123, 5),
+					NewMapping(30, 130, 10),
+				},
+			},
+			args: args{
+				srcStart: 15,
+				length:   20,
+			},
+			want: [][]int{
+				{115, 5},
+				{20, 3},
+				{123, 5},
+				{28, 2},
+				{130, 5},
+			},
+			want1: "dst",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Mapper{
+				Src:      tt.fields.Src,
+				Dst:      tt.fields.Dst,
+				mappings: tt.fields.mappings,
+			}
+			got, got1 := m.MapRange(tt.args.srcStart, tt.args.length)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MapRange() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("MapRange() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
