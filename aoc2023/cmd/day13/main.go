5dabe38d55598eb76479f2f92c684fd6a0581728
Andy Nguyen <anguyen@computer.org>
2025-01-01 18:53:52 -0500

    day 13 part 2

--- aoc2023/cmd/day13/main.go
+++ aoc2023/cmd/day13/main.go
@@ -28,17 +28,14 @@ func part1(input string) {
 		}
 
 		// look for horizontal reflection
-		if found, row1, row2 := findReflection(grid); found {
-			fmt.Println("horizontal reflection found", row1, row2)
+		if found, _, row2 := findReflection(grid); found {
 			total += 100 * row2
 			continue
 		}
-		fmt.Println("no horizontal reflection found")
 
 		// look for vertical reflection
 		tgrid := transpose(grid)
-		if found, col1, col2 := findReflection(tgrid); found {
-			fmt.Println("vertical reflection found", col1, col2)
+		if found, _, col2 := findReflection(tgrid); found {
 			total += col2
 			continue
 		}
@@ -78,5 +75,75 @@ func transpose(grid [][]rune) [][]rune {
 }
 
 func part2(input string) {
+	total := 0
+	patterns := strings.Split(input, "\n\n")
+	for _, pattern := range patterns {
+		var grid [][]rune
+		lines := strings.Split(pattern, "\n")
+		for _, line := range lines {
+			grid = append(grid, []rune(line))
+		}
 
+		// look for horizontal reflection
+		if found, row1, row2 := findReflectionSmudge(grid); found {
+			fmt.Println("horizontal reflection found", row1, row2)
+			total += 100 * row2
+			continue
+		}
+		fmt.Println("no horizontal reflection found")
+
+		// look for vertical reflection
+		tgrid := transpose(grid)
+		if found, col1, col2 := findReflectionSmudge(tgrid); found {
+			fmt.Println("vertical reflection found", col1, col2)
+			total += col2
+			continue
+		}
+		panic("no horizontal or vertical reflection found")
+	}
+
+	slog.Info("Part 2:", "total", total)
+}
+
+func findReflectionSmudge(grid [][]rune) (bool, int, int) {
+	for row := 1; row < len(grid); row++ {
+		smudge := compareSmudge(grid[row], grid[row-1])
+		if string(grid[row]) == string(grid[row-1]) || smudge {
+			reflection := true
+			for prevRow, nextRow := row-2, row+1; prevRow >= 0 && nextRow < len(grid); prevRow, nextRow = prevRow-1, nextRow+1 {
+				if string(grid[prevRow]) == string(grid[nextRow]) {
+					continue
+				}
+				if !smudge && compareSmudge(grid[prevRow], grid[nextRow]) {
+					smudge = true
+					continue
+				}
+				reflection = false
+				break
+			}
+			if reflection && smudge {
+				return true, row - 1, row
+			}
+		}
+	}
+	return false, 0, 0
+}
+
+func compareSmudge(row1, row2 []rune) bool {
+	rocks := make([]int, len(row1))
+	for i := range rocks {
+		if row1[i] == '#' {
+			rocks[i]++
+		}
+		if row2[i] == '#' {
+			rocks[i]++
+		}
+	}
+	count := 0
+	for _, n := range rocks {
+		if n == 1 {
+			count++
+		}
+	}
+	return count == 1
 }
