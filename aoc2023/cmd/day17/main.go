1b3de924d390b99dc09225e89bc9a06c8b34ffef
Andy Nguyen <anguyen@computer.org>
2025-01-05 11:56:50 -0500

    day 17 part 2

--- aoc2023/cmd/day17/main.go
+++ aoc2023/cmd/day17/main.go
@@ -18,6 +18,14 @@ func main() {
 
 }
 
+var (
+	up         = [2]int{-1, 0}
+	down       = [2]int{1, 0}
+	left       = [2]int{0, -1}
+	right      = [2]int{0, 1}
+	directions = [][2]int{up, down, left, right}
+)
+
 func part1(input string) {
 	var grid [][]int
 	for _, line := range strings.Split(input, "\n") {
@@ -28,11 +36,6 @@ func part1(input string) {
 		grid = append(grid, row)
 	}
 
-	up := [2]int{-1, 0}
-	down := [2]int{1, 0}
-	left := [2]int{0, -1}
-	right := [2]int{0, 1}
-	directions := [][2]int{up, down, left, right}
 	pq := queue.NewPriorityQueue()
 	queue.Push(pq, queue.Item{0, 0, 0, 0, 0, 0})
 	seen := make(map[[5]int]bool)
@@ -40,6 +43,7 @@ func part1(input string) {
 		item := queue.Pop(pq)
 		hl, r, c, dr, dc, steps := item[0], item[1], item[2], item[3], item[4], item[5]
 
+		// check if we've reached destination
 		if r == len(grid)-1 && c == len(grid[0])-1 {
 			slog.Info("Part 1:", "heat loss", hl)
 			break
@@ -77,5 +81,63 @@ func part1(input string) {
 }
 
 func part2(input string) {
+	var grid [][]int
+	for _, line := range strings.Split(input, "\n") {
+		var row []int
+		for _, ch := range line {
+			row = append(row, int(ch-'0'))
+		}
+		grid = append(grid, row)
+	}
 
+	pq := queue.NewPriorityQueue()
+	queue.Push(pq, queue.Item{0, 0, 0, 0, 0, 0})
+	seen := make(map[[5]int]bool)
+	for pq.Len() > 0 {
+		item := queue.Pop(pq)
+		hl, r, c, dr, dc, steps := item[0], item[1], item[2], item[3], item[4], item[5]
+
+		// check if we've reached destination with at least 4 steps in the same direction
+		if r == len(grid)-1 && c == len(grid[0])-1 && steps >= 4 {
+			slog.Info("Part 2:", "heat loss", hl)
+			break
+		}
+
+		key := [5]int{r, c, dr, dc, steps}
+		if seen[key] {
+			continue
+		}
+		seen[key] = true
+
+		for _, dir := range directions {
+			nr := r + dir[0]
+			nc := c + dir[1]
+			// check bounds
+			if nr < 0 || nr >= len(grid) || nc < 0 || nc >= len(grid[0]) {
+				continue
+			}
+			// cannot reverse direction
+			if (dr != 0 && dir[0] == -dr) || (dc != 0 && dir[1] == -dc) {
+				continue
+			}
+			// ensure we move at least 4 steps in the same direction
+			// special case at beginning when steps == 0
+			if steps < 4 && steps > 0 {
+				if dir[0] == dr && dir[1] == dc {
+					queue.Push(pq, queue.Item{hl + grid[nr][nc], nr, nc, dir[0], dir[1], steps + 1})
+				}
+				continue
+			}
+			// check neighbor in the same direction
+			if dir[0] == dr && dir[1] == dc {
+				// cannot move more than 10 steps in same direction
+				if steps == 10 {
+					continue
+				}
+				queue.Push(pq, queue.Item{hl + grid[nr][nc], nr, nc, dir[0], dir[1], steps + 1})
+			} else {
+				queue.Push(pq, queue.Item{hl + grid[nr][nc], nr, nc, dir[0], dir[1], 1})
+			}
+		}
+	}
 }
