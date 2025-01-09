dadadf266e6a9860d2a18364ec04edb32ff09c9d
Andy Nguyen <anguyen@computer.org>
2025-01-09 17:32:48 -0500

    day 19 part 2

--- aoc2023/cmd/day19/main.go
+++ aoc2023/cmd/day19/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 )
@@ -47,17 +46,17 @@ type Rule struct {
 }
 
 func NewRule(raw string) *Rule {
-	re := regexp.MustCompile(`^(.+)([<>])(.+):(.+)$`)
 	r := &Rule{}
-	match := re.FindStringSubmatch(raw)
-	if match == nil {
+	// e.g., s>2770:qs
+	parts := strings.Split(raw, ":")
+	if len(parts) == 1 {
 		r.Destination = raw
 		return r
 	}
-	r.Operand1 = match[1]
-	r.Operation = match[2]
-	r.Operand2, _ = strconv.Atoi(match[3])
-	r.Destination = match[4]
+	r.Operand1 = string(parts[0][0])
+	r.Operation = string(parts[0][1])
+	r.Operand2, _ = strconv.Atoi(parts[0][2:])
+	r.Destination = parts[1]
 	return r
 }
 
@@ -82,42 +81,23 @@ func (r *Rule) Apply(part map[string]int) (string, bool) {
 func part1(input string) {
 	inputParts := strings.Split(input, "\n\n")
 
-	workflows := make(map[string][]*Rule)
-	reWorkflow := regexp.MustCompile(`^(.+){(.+)}$`)
-	for _, line := range strings.Split(inputParts[0], "\n") {
-		match := reWorkflow.FindStringSubmatch(line)
-		rules := strings.Split(match[2], ",")
-		for _, rule := range rules {
-			r := NewRule(rule)
-			workflows[match[1]] = append(workflows[match[1]], r)
-		}
-	}
-
-	//for name, rules := range workflows {
-	//	s := fmt.Sprintf("%s:", name)
-	//	for _, rule := range rules {
-	//		s = fmt.Sprintf("%s %v", s, rule)
-	//	}
-	//	fmt.Println(s)
-	//}
+	workflows := buildWorkflows(inputParts[0])
 
 	sum := 0
 	rejected := 0
 	for _, line := range strings.Split(inputParts[1], "\n") {
 		var x, m, a, s int
-		fmt.Sscanf(line, "{x=%d,m=%d,a=%d,s=%d}", &x, &m, &a, &s)
+		_, _ = fmt.Sscanf(line, "{x=%d,m=%d,a=%d,s=%d}", &x, &m, &a, &s)
 		part := map[string]int{
 			"x": x,
 			"m": m,
 			"a": a,
 			"s": s,
 		}
-		//fmt.Println(part)
 		q := []string{"in"}
 		for len(q) > 0 {
 			workflow := q[0]
 			q = q[1:]
-			//fmt.Println(workflow)
 			if workflow == "A" {
 				sum += part["x"] + part["m"] + part["a"] + part["s"]
 				break
@@ -140,6 +120,87 @@ func part1(input string) {
 	slog.Info("Part 1:", "sum", sum)
 }
 
-func part2(input string) {
+func buildWorkflows(input string) map[string][]*Rule {
+	workflows := make(map[string][]*Rule)
+	for _, line := range strings.Split(input, "\n") {
+		// e.g., qqz{s>2770:qs,m<1801:hdj,R}
+		parts := strings.Split(line[:len(line)-1], "{")
+		name := parts[0]
+		rules := strings.Split(parts[1], ",")
+		for _, rule := range rules {
+			r := NewRule(rule)
+			workflows[name] = append(workflows[name], r)
+		}
+	}
+	return workflows
+}
 
+func part2(input string) {
+	inputParts := strings.Split(input, "\n\n")
+
+	workflows := buildWorkflows(inputParts[0])
+	var paths [][]string
+
+	q := [][]string{{"0", "in"}}
+	for len(q) > 0 {
+		n, _ := strconv.Atoi(q[0][0])
+		workflow := q[0][1]
+		path := q[0][2:]
+		q = q[1:]
+
+		if workflow == "A" {
+			paths = append(paths, append(path, strconv.Itoa(n), "A"))
+		}
+
+		if workflow == "R" {
+			continue
+		}
+
+		for i, rule := range workflows[workflow] {
+			newpath := append(path, strconv.Itoa(n), workflow)
+			next := append([]string{strconv.Itoa(i), rule.Destination}, newpath...)
+			q = append(q, next)
+		}
+	}
+
+	total := 0
+	for _, path := range paths {
+		part := map[string][]int{
+			"x": {1, 4000},
+			"m": {1, 4000},
+			"a": {1, 4000},
+			"s": {1, 4000},
+		}
+		for i := 1; i < len(path)-1; i += 2 {
+			workflow := path[i]
+			n, _ := strconv.Atoi(path[i+1])
+			next := path[i+2]
+
+			rules := workflows[workflow]
+			for j, rule := range rules {
+				if rule.Destination == next && n == j {
+					if len(rule.Operation) > 0 {
+						if rule.Operation == "<" {
+							part[rule.Operand1][1] = min(part[rule.Operand1][1], rule.Operand2-1)
+						} else { // >
+							part[rule.Operand1][0] = max(part[rule.Operand1][0], rule.Operand2+1)
+						}
+					}
+					break
+				} else {
+					if rule.Operation == "<" { // >=
+						part[rule.Operand1][0] = max(part[rule.Operand1][0], rule.Operand2)
+					} else { // <=
+						part[rule.Operand1][1] = min(part[rule.Operand1][1], rule.Operand2)
+					}
+				}
+			}
+		}
+		combinations := (part["x"][1] - part["x"][0] + 1) *
+			(part["m"][1] - part["m"][0] + 1) *
+			(part["a"][1] - part["a"][0] + 1) *
+			(part["s"][1] - part["s"][0] + 1)
+		total += combinations
+	}
+	slog.Info("Part 2:", "total combinations", total)
 }
