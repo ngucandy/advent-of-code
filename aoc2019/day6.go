2dde931211b2d21e6bc20cb5aa24d02f55473a4b
Andy Nguyen <anguyen@computer.org>
2025-01-13 10:56:38 -0500

    day 6

---
+++ aoc2019/day6.go
@@ -0,0 +1,105 @@
+package aoc2019
+
+import (
+	"fmt"
+	"strings"
+)
+
+func init() {
+	DayMap["6"] = Day6{
+		`COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L`,
+		`COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN`,
+	}
+}
+
+type Day6 struct {
+	example1 string
+	example2 string
+}
+
+func (d Day6) Part1(input string) {
+	orbits := make(map[string]string)
+	for _, line := range strings.Split(input, "\n") {
+		parts := strings.Split(line, ")")
+		orbits[parts[1]] = parts[0]
+	}
+
+	total := 0
+	cache := make(map[string]int)
+	for orbiter := range orbits {
+		total += d.countOrbits(orbiter, orbits, cache)
+	}
+	fmt.Println("part1", total)
+}
+
+type Day6Tuple struct {
+	name string
+	cost int
+}
+
+func (d Day6) Part2(input string) {
+	orbits := make(map[string][]string)
+	for _, line := range strings.Split(input, "\n") {
+		parts := strings.Split(line, ")")
+		orbits[parts[0]] = append(orbits[parts[0]], parts[1])
+		orbits[parts[1]] = append(orbits[parts[1]], parts[0])
+	}
+
+	q := []Day6Tuple{{"YOU", 0}}
+	seen := make(map[string]int)
+	for len(q) > 0 {
+		t := q[0]
+		q = q[1:]
+
+		if t.name == "SAN" {
+			fmt.Println("part2", t.cost-2)
+			break
+		}
+
+		if seenCost, found := seen[t.name]; found && seenCost < t.cost {
+			continue
+		}
+		seen[t.name] = t.cost
+
+		for _, neighbor := range orbits[t.name] {
+			q = append(q, Day6Tuple{neighbor, t.cost + 1})
+		}
+	}
+}
+
+func (d Day6) countOrbits(orbiter string, orbits map[string]string, cache map[string]int) int {
+	orbitee, exists := orbits[orbiter]
+	if !exists {
+		return 0
+	}
+
+	if count, exists := cache[orbiter]; exists {
+		return count
+	}
+
+	orbitCount := 1 + d.countOrbits(orbitee, orbits, cache)
+	cache[orbiter] = orbitCount
+	return orbitCount
+}
