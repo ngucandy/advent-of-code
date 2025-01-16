1020a775619f8faac03c1ae63801c4e61b48468a
Andy Nguyen <anguyen@computer.org>
2025-01-15 20:27:17 -0500

    refactor day11

---
+++ aoc2024/day11.go
@@ -0,0 +1,66 @@
+package aoc2024
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func init() {
+	DayMap["11"] = Day11{}
+}
+
+type Day11 struct {
+	example string
+}
+
+func (d Day11) Part1(input string) {
+	var stones []int
+	for _, stone := range strings.Split(strings.TrimSpace(input), " ") {
+		n, _ := strconv.Atoi(stone)
+		stones = append(stones, n)
+	}
+	count := 0
+	cache := make(map[[2]int]int)
+	for _, stone := range stones {
+		count += blink(stone, 25, cache)
+	}
+	fmt.Println("part1", count)
+}
+
+func (d Day11) Part2(input string) {
+	var stones []int
+	for _, stone := range strings.Split(strings.TrimSpace(input), " ") {
+		n, _ := strconv.Atoi(stone)
+		stones = append(stones, n)
+	}
+	count := 0
+	cache := make(map[[2]int]int)
+	for _, stone := range stones {
+		count += blink(stone, 75, cache)
+	}
+	fmt.Println("part2", count)
+}
+
+func blink(stone int, times int, cache map[[2]int]int) int {
+	if times == 0 {
+		return 1
+	}
+	if count, ok := cache[[2]int{stone, times}]; ok {
+		return count
+	}
+
+	if stone == 0 {
+		cache[[2]int{stone, times}] = blink(1, times-1, cache)
+		return cache[[2]int{stone, times}]
+	}
+	s := strconv.Itoa(stone)
+	if len(s)%2 == 0 {
+		n1, _ := strconv.Atoi(s[:len(s)/2])
+		n2, _ := strconv.Atoi(s[len(s)/2:])
+		cache[[2]int{stone, times}] = blink(n1, times-1, cache) + blink(n2, times-1, cache)
+		return cache[[2]int{stone, times}]
+	}
+	cache[[2]int{stone, times}] = blink(stone*2024, times-1, cache)
+	return cache[[2]int{stone, times}]
+}
