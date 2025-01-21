4cac8eaaa3c7407886c9f8e6dfcb46d521f3a814
Andy Nguyen <anguyen@computer.org>
2025-01-21 18:23:39 -0500

    2019 day12 part 2

--- aoc2019/day12.go
+++ aoc2019/day12.go
@@ -3,6 +3,7 @@ package aoc2019
 import (
 	"cmp"
 	"github.com/ngucandy/advent-of-code/internal/helpers"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -67,5 +68,67 @@ func (d Day12) Part1(input string) any {
 }
 
 func (d Day12) Part2(input string) any {
-	return "no answer yet"
+	var positions, velocities, oPositions, oVelocities [][]int
+	for _, line := range strings.Split(input, "\n") {
+		position := make([]int, 3)
+		velocity := make([]int, 3)
+		parts := strings.Split(line[1:len(line)-1], ", ")
+		position[0], _ = strconv.Atoi(parts[0][2:])
+		position[1], _ = strconv.Atoi(parts[1][2:])
+		position[2], _ = strconv.Atoi(parts[2][2:])
+		positions = append(positions, position)
+		velocities = append(velocities, velocity)
+		oPositions = append(oPositions, slices.Clone(position))
+		oVelocities = append(oVelocities, slices.Clone(velocity))
+	}
+
+	columnEquals := func(c int, a, b [][]int) bool {
+		for r := range a {
+			if a[r][c] != b[r][c] {
+				return false
+			}
+		}
+		return true
+	}
+
+	// Look for independent cycles of x, y and z across all positions and
+	// velocities. In other words, when all x values (index 0) return to
+	// their original values, that counts as a cycle for x. Same applies
+	// for y and z. Look for 2 cycles before stopping.
+	var cycles [3][]int
+step:
+	for n := 1; len(cycles[0]) < 2 || len(cycles[1]) < 2 || len(cycles[2]) < 2; n++ {
+		// apply gravity
+		for i, pos1 := range positions[:len(positions)-1] {
+			for j, pos2 := range positions[i+1:] {
+				velocities[i][0] += cmp.Compare(pos2[0], pos1[0])
+				velocities[i+j+1][0] += cmp.Compare(pos1[0], pos2[0])
+				velocities[i][1] += cmp.Compare(pos2[1], pos1[1])
+				velocities[i+j+1][1] += cmp.Compare(pos1[1], pos2[1])
+				velocities[i][2] += cmp.Compare(pos2[2], pos1[2])
+				velocities[i+j+1][2] += cmp.Compare(pos1[2], pos2[2])
+			}
+		}
+
+		// apply velocity
+		for i := range positions {
+			positions[i][0] += velocities[i][0]
+			positions[i][1] += velocities[i][1]
+			positions[i][2] += velocities[i][2]
+		}
+
+		for c := range cycles {
+			if len(cycles[c]) >= 2 {
+				continue
+			}
+			if !columnEquals(c, oPositions, positions) || !columnEquals(c, oVelocities, velocities) {
+				continue step
+			}
+			cycles[c] = append(cycles[c], n)
+		}
+	}
+
+	// use the difference between the first and second cycles as the step
+	// value for repeating the cycle
+	return helpers.LCM(cycles[0][1]-cycles[0][0], cycles[1][1]-cycles[1][0], cycles[2][1]-cycles[2][0])
 }
