7bce92812f0b54c4cb5583a94776ee53ee5c8b32
Andy Nguyen <anguyen@computer.org>
2025-01-21 13:35:00 -0500

    refactor day25

---
+++ aoc2024/day25.go
@@ -0,0 +1,89 @@
+package aoc2024
+
+import (
+	"fmt"
+	"strings"
+)
+
+func init() {
+	Days["25"] = Day25{
+		eg1: `#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####`,
+	}
+}
+
+type Day25 struct {
+	eg1, eg2 string
+}
+
+func (d Day25) Part1(input string) {
+	var locks, keys [][5]int
+	for _, section := range strings.Split(input, "\n\n") {
+		heights := [5]int{-1, -1, -1, -1, -1}
+		for _, line := range strings.Split(section, "\n") {
+			for i, char := range line {
+				if char == '#' {
+					heights[i]++
+				}
+			}
+		}
+		if section[:5] == "#####" {
+			locks = append(locks, heights)
+		} else {
+			keys = append(keys, heights)
+		}
+	}
+
+	count := 0
+	for _, lock := range locks {
+	KeyLoop:
+		for _, key := range keys {
+			for i := range key {
+				if lock[i]+key[i] > 5 {
+					continue KeyLoop
+				}
+			}
+			count++
+		}
+	}
+	fmt.Println("part1", count)
+}
+
+func (d Day25) Part2(_ string) {}
