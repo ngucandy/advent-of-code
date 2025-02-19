f02a81c3bc79747c79441dfe5969d99bfce9d778
Andy Nguyen <anguyen@computer.org>
2025-02-18 22:39:23 -0500

    2019 day 19 part 1

---
+++ aoc2019/day19.go
@@ -0,0 +1,33 @@
+package aoc2019
+
+import "slices"
+
+func init() {
+	Days["19"] = &Day19{}
+}
+
+type Day19 struct {
+	eg1, eg2 string
+}
+
+func (d Day19) Part1(input string) any {
+	program := ParseIntcodeProgram(input)
+	count := 0
+	n := 50
+	for x := range n {
+		for y := range n {
+			c := NewIntcodeComputer(slices.Clone(program), []int{x, y})
+			for len(c.output) == 0 {
+				c.Step()
+			}
+			if c.output[0] == 1 {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func (d Day19) Part2(input string) any {
+	return "no answer yet"
+}
