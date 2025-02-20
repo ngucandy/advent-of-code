7f86e6459b38960b2a6b2539d86680b061af94c8
Andy Nguyen <anguyen@computer.org>
2025-02-20 09:57:55 -0500

    2019 day 21 part 2

--- aoc2019/day21.go
+++ aoc2019/day21.go
@@ -25,7 +25,7 @@ WALK
 	for c.Step() {
 	}
 	for _, n := range c.output {
-		if n > 255 {
+		if n > 127 {
 			return n
 		}
 		fmt.Printf("%c", n)
@@ -34,5 +34,29 @@ WALK
 }
 
 func (d Day21) Part2(input string) any {
+	program := `NOT A J
+AND D J
+NOT C T
+AND D T
+AND H T
+OR T J
+NOT B T
+AND D T
+OR T J
+RUN
+`
+	var in []int
+	for _, ch := range program {
+		in = append(in, int(ch))
+	}
+	c := NewIntcodeComputer(ParseIntcodeProgram(input), in)
+	for c.Step() {
+	}
+	for _, n := range c.output {
+		if n > 127 {
+			return n
+		}
+		fmt.Printf("%c", n)
+	}
 	return "no answer yet"
 }
