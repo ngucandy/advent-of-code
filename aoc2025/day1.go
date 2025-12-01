f4eadc714d22d79db3bce67bb4624c901472f0f7
Andy Nguyen <anguyen@computer.org>
2025-12-01 17:59:26 -0500

    day 1 part 2

--- aoc2025/day1.go
+++ aoc2025/day1.go
@@ -47,6 +47,37 @@ func (d Day1) Part1(input string) any {
 	return count
 }
 
-func (d Day1) Part2(s string) any {
-	return nil
+func (d Day1) Part2(input string) any {
+	size := 100
+	count := 0
+	dial := 50
+	var rotation string
+	var distance int
+	for _, line := range strings.Split(input, "\n") {
+		_, err := fmt.Sscanf(line, "%1s%d", &rotation, &distance)
+		if err != nil {
+			fmt.Printf("error scanning input data: %v\n", err)
+			break
+		}
+
+		// increase count for every full rotation
+		if distance > size {
+			count += distance / size
+			distance %= size
+		}
+
+		if rotation == "L" {
+			if dial > 0 && distance >= dial {
+				count++
+			}
+			distance = -distance + size
+		} else {
+			if distance >= size-dial {
+				count++
+			}
+		}
+		dial = (dial + distance) % size
+		fmt.Println(line, dial, count)
+	}
+	return count
 }
