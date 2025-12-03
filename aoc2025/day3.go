9bced51c49af0e7df188b21a671b10d84a22b253
Andy Nguyen <anguyen@computer.org>
2025-12-03 10:45:36 -0500

    day 3 part 1

---
+++ aoc2025/day3.go
@@ -0,0 +1,51 @@
+package aoc2025
+
+import (
+	"strings"
+)
+
+func init() {
+	Days["3"] = Day3{
+		example: `987654321111111
+811111111111119
+234234234234278
+818181911112111`,
+	}
+}
+
+type Day3 struct {
+	example string
+}
+
+func (d Day3) Part1(input string) any {
+	total := 0
+	n1, n2 := -1, -1
+	for _, line := range strings.Split(input, "\n") {
+		for _, n := range "987654321" {
+			i := strings.Index(line, string(n))
+			if i == -1 || i == len(line)-1 {
+				continue
+			}
+			n1 = int(n - '0')
+			line = line[i+1:]
+			break
+		}
+
+		for _, n := range "9876543210" {
+			i := strings.Index(line, string(n))
+			if i == -1 {
+				continue
+			}
+			n2 = int(n - '0')
+			break
+		}
+
+		total += (n1 * 10) + n2
+	}
+	return total
+}
+
+func (d Day3) Part2(input string) any {
+	//TODO implement me
+	return nil
+}
