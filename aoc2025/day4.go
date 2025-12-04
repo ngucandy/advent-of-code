6df0926899248a966263c282c1150c5999de2490
Andy Nguyen <anguyen@computer.org>
2025-12-04 09:54:03 -0500

    day 4 part 2

--- aoc2025/day4.go
+++ aoc2025/day4.go
@@ -61,6 +61,44 @@ func (d Day4) parseGrid(input string) [][]rune {
 }
 
 func (d Day4) Part2(input string) any {
-	//TODO implement me
-	return nil
+	total := 0
+	grid := d.parseGrid(input)
+	// loop through removing rolls until we're unable to remove more
+	for removed := d.removeRolls(grid); removed > 0; removed = d.removeRolls(grid) {
+		total += removed
+	}
+	return total
+}
+
+// removeRolls modifies the given grid by removing rolls that are removable
+// and returns the number of rolls removed
+func (d Day4) removeRolls(grid [][]rune) int {
+	var removable [][2]int
+	for r, row := range grid {
+		for c, ch := range row {
+			if ch == '.' {
+				continue
+			}
+			// found a roll, count adjacent rolls
+			adjacent := 0
+			for _, drc := range [][2]int{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}} {
+				ar := r + drc[0]
+				ac := c + drc[1]
+				if ar < 0 || ar >= len(grid) || ac < 0 || ac >= len(grid[r]) {
+					// out of grid bounds
+					continue
+				}
+				if grid[ar][ac] == '@' {
+					adjacent++
+				}
+			}
+			if adjacent < 4 {
+				removable = append(removable, [2]int{r, c})
+			}
+		}
+	}
+	for _, point := range removable {
+		grid[point[0]][point[1]] = '.'
+	}
+	return len(removable)
 }
