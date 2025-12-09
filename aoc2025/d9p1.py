8aaf9ab579c7cb444d3bdba2aa6db79ade377828
Andy Nguyen <anguyen@computer.org>
2025-12-09 06:40:34 -0500

    day 9 part 1 python

---
+++ aoc2025/d9p1.py
@@ -0,0 +1,7 @@
+from itertools import combinations
+
+tiles = [tuple(map(int, line.strip().split(','))) for line in open(0)]
+pairs = list(combinations(tiles, 2))
+area = [(abs(p1[0]-p2[0])+1) * (abs(p1[1]-p2[1])+1) for p1, p2 in pairs]
+
+print(sorted(area, reverse=True)[0])
